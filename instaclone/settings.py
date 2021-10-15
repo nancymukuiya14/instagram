ac1b52c6ce9699db81856848d75ecf4ce6ec41ec
nancy.kigotho@student.moringaschool.com <nancykigotho14@gmail.com>
2021-10-15 23:13:11 +0300

    update the navbar

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'instagram',
+    'crispy_forms'
     
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 MIDDLEWARE = [
