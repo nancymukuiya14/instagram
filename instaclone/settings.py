0323d27ea2e2c358a5f4a9bb0176969bd9d9ebfd
nancy.kigotho@student.moringaschool.com <nancykigotho14@gmail.com>
2021-10-17 02:00:16 +0300

    'ignore the env file

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -102,17 +102,7 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=config('DATABASE_URL')
-#     )
-# }
 
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
-
-# Password validation
-# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
